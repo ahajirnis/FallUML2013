cd6967abf05a45136cefb02762ed4cec896c99c1
Prasanna Shrestha <shrestha.pr@husky.neu.edu>
2013-10-15 23:53:58 -0400

    Created Model for New Tables: Policy, DiagramContext, DiagramPolicyScore

---
+++ src/domain/Policy.java
@@ -0,0 +1,43 @@
+package domain;
+
+public class Policy {
+	
+	private int policyID;
+	private String policyName;
+	private String policyDescription;
+	private int policyLevel;
+	
+	public int getPolicyID() {
+		return policyID;
+	}
+	
+	public void setPolicyID(int policyID) {
+		this.policyID = policyID;
+	}
+	
+	public String getPolicyName() {
+		return policyName;
+	}
+	
+	public void setPolicyName(String policyName) {
+		this.policyName = policyName;
+	}
+	
+	public String getPolicyDescription() {
+		return policyDescription;
+	}
+	
+	public void setPolicyDescription(String policyDescription) {
+		this.policyDescription = policyDescription;
+	}
+	
+	public int getPolicyLevel() {
+		return policyLevel;
+	}
+	
+	public void setPolicyLevel(int policyLevel) {
+		this.policyLevel = policyLevel;
+	}
+	
+
+}
