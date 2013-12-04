3e4627d2d3added2afd16a799a0d78ea9b65fab9
kongw <kong.w@husky.neu.edu>
2013-12-03 20:18:46 -0500

    delete error

--- src/repository/PolicyDAO.java
+++ src/repository/PolicyDAO.java
@@ -51,34 +51,7 @@ public class PolicyDAO {
 	return policy;
     }
     
-    // create Metric Dao
-    public static Policy addPolicy(Policy policy) {
-    	ResultSet rs;
-	try {
-		Connection conn = DbManager.getConnection();		
-	    PreparedStatement pstmt = conn.prepareStatement(
-		    "INSERT into Metric(policyName, policyDescription,policyLevel) VALUES(?,?,?);");
-	    pstmt.setString(1, policy.getPolicyName());
-		pstmt.setString(2, policy.getPolicyDescription());
-		pstmt.setInt(3, policy.getPolicyLevel());
-		
-	    // Execute the SQL statement and update database accordingly.
-	    pstmt.executeUpdate();
-	    rs = pstmt.getGeneratedKeys();
-	    if (rs.next()) {
-		int newId = rs.getInt(1);
-		policy.setPolicyID(newId);
-	    }
-	    pstmt.close();
-	    rs.close();
-	    conn.close();
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	    return null;
-	}
-	return policy;
-    }
-     
+   
     
     
     
