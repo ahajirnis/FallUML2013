15c5fc5a0aaac1dfd26e3f68bf88f77bd6cb11d0
kongw <kong.w@husky.neu.edu>
2013-12-04 16:25:45 -0500

    The prepareStatement has SQL Syntax Error. Error one: "... VALUES(?,?,?,?)" Correct one "... VALUES(?,?,?)" Only three attributes needed.
    Call MetricDAO's AddMetric method in CreatePolicy.java
    
    Add name for Attribute Input Tag

--- src/controller/CreatePolicy.java
+++ src/controller/CreatePolicy.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import repository.MetricsDAO;
 import repository.PolicyDAO;
 import repository.ProjectDAO;
+import domain.Attributes;
 import domain.Policy;
 import domain.Project;
 
@@ -39,12 +41,42 @@ public class CreatePolicy extends HttpServlet {
 		String description = request.getParameter("PolicyDescription");	
 		String levels = request.getParameter("PolicyLevel");
 		int level = Integer.parseInt(levels);
+		
+		String metric = request.getParameter("metric");
+		
 		Policy policy = new Policy();
 		policy.setPolicyName(name); 
 		policy.setPolicyLevel(level);
 		policy.setPolicyDescription(description);
 		
 		PolicyDAO.addPolicy(policy);
+		
+		if (metric.equals("Attribute")){
+			String idealAttribute = request.getParameter("addIdealAttribute");
+			int ideal = Integer.parseInt(idealAttribute);
+			String maxAttribute = request.getParameter("addMaxlAttribute");
+			int max = Integer.parseInt(maxAttribute);
+			String minAttribute = request.getParameter("addMinlAttribute");
+			int min = Integer.parseInt(minAttribute);
+			Attributes attribute = new Attributes();
+			attribute.setIdealNoOfAttributes(ideal);
+			attribute.setMaxNoOfAttributes(max);
+			attribute.setMinNoOfAttributes(min);
+			
+			// attribute metric type id is 3 
+			attribute.setMetricTypeId(3);
+			
+			// hard coding for test
+			attribute.setMetricsWeight(1);
+			attribute.setPolicyId(1);
+			
+	//		attribute.setPolicyId(policyId);
+			try {
+				MetricsDAO.addMetrics(attribute);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		try {
 		Success(request, response);	
 		} catch (Exception e) {
